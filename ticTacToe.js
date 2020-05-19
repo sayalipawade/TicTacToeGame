e22750b0299e761ec1615dceca416d1c2424f7a4
sayalipawade <sayalipawade05@gmail.com>
2020-05-19 23:02:44 +0530

    Added:Starting game with Toss

--- ticTacToe.js
+++ ticTacToe.js
@@ -12,17 +12,34 @@ function displayBoard()
 {
 	for(let i=0;i<7;i=i+3 )
 	{
-		console.log("|"+ board[i]+"|"+board[i+1]+"|"+board[i+2]+"|")
+        console.log("|"+ board[i]+"|"+board[i+1]+"|"+board[i+2]+"|")
     }
 }
+
 /**
  * assign value to user
  */ 
 function assignSymbol()
 {
-		player="user"
-		console.log("Your symbol is X")
-		user="X"
-		computer="O"
+	player="user"
+	console.log("Your symbol is X")
+	user="X"
+	computer="O"
+}
+
+/**
+ * To know who play first
+ */
+function switchPlayer()
+{
+	toss=Math.floor(Math.random()%2)
+	if(toss==0)
+	{
+		userPlay()
+    }
+    else
+    {
+		computerPlay()
+    }
 }
 displayBoard()
