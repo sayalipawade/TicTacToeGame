d9d98f7e4c420df35d9e589f9f506adf280d6672
sayalipawade <sayalipawade05@gmail.com>
2020-05-19 22:47:54 +0530

    Added:Starting with fresh board

--- ticTacToe.js
+++ ticTacToe.js
@@ -1,1 +1,18 @@
 console.log("********Welcome to Tic Tac Toe Game*******")
+
+/**
+ * array of board
+ */
+let board=[1,2,3,4,5,6,7,8,9];
+
+/**
+ * display board
+ */
+function displayBoard()
+{
+	for(let i=0;i<7;i=i+3 )
+	{
+		console.log("|"+ board[i]+"|"+board[i+1]+"|"+board[i+2]+"|")
+    }
+}
+displayBoard()
