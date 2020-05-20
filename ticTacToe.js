c53198aabbc43803a8de5841361b822560df1aa1
sayalipawade <sayalipawade05@gmail.com>
2020-05-20 07:41:17 +0530

    Added:Display board so user can choose valid cell

--- ticTacToe.js
+++ ticTacToe.js
@@ -1,9 +1,15 @@
-console.log("********Welcome to Tic Tac Toe Game*******")
+console.log("Welcome to Tic Tac Toe")
+let read=require('readline-sync')
 
 /**
- * array of board
+ * variable
  */
+let cellCount=0
+let maximumCell=9
+let user="X"
+
 let board=[1,2,3,4,5,6,7,8,9];
+
 
 /**
  * display board
@@ -12,19 +18,18 @@ function displayBoard()
 {
 	for(let i=0;i<7;i=i+3 )
 	{
-        console.log("|"+ board[i]+"|"+board[i+1]+"|"+board[i+2]+"|")
+		console.log("|"+ board[i]+"|"+board[i+1]+"|"+board[i+2]+"|")
     }
 }
-
 /**
  * assign value to user
- */ 
+ */
 function assignSymbol()
 {
-	player="user"
-	console.log("Your symbol is X")
-	user="X"
-	computer="O"
+		player="user"
+		console.log("Your symbol is X")
+		user="X"
+		computer="O"
 }
 
 /**
@@ -42,4 +47,32 @@ function switchPlayer()
 		computerPlay()
     }
 }
+/**
+ * user play
+ */
+function userPlay()
+{
+	if(cellCount < maximumCell)
+	{
+		 position=read.question("Enter Number Between 1 to 9:")
+		if(board[position-1] == position )
+		{
+			board[position-1]=user
+			cellCount++ 
+			displayBoard()
+        }
+        else
+        {
+	        console.log("Invalid Cell")
+			userPlay()
+        }
+    }
+    else
+    {
+		console.log("Game tie !!")
+		process.exit();
+    }
+}
 displayBoard()
+assignSymbol()
+switchPlayer()
